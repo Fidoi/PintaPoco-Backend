70eecb659be162f0572f109def4863fac76f94a6
Fidoi <elautoricoi@gmail.com>
2025-02-04 23:35:45 -0300

    olvide un process.env

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ cloudinary.config({
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3002',
+    origin: `${process.env.URL_FRONTEND}`,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'x-api-key'],
   });
