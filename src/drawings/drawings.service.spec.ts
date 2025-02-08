bd42aa4ff95ace27399282754c5324403645df9c
Fidoi <elautoricoi@gmail.com>
2025-02-07 23:07:08 -0300

    Nuevo deploy

--- src/drawings/drawings.service.spec.ts
+++ src/drawings/drawings.service.spec.ts
@@ -5,7 +5,6 @@ import { DrawingsService } from './drawings.service';
 describe('DrawingsController', () => {
   let controller: DrawingsController;
 
-  // Creamos un mock para DrawingsService (puede ser un objeto vacío si solo verificamos la existencia del controlador)
   const mockDrawingsService = {};
 
   beforeEach(async () => {
